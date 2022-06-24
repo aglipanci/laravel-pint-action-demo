762e8a804fe1ee5b7bf81e2a1825b8b941f4dab6
Agli Pançi <aglipanci@users.noreply.github.com>
2022-06-24 13:55:51 +0200

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,5 +9,6 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
+    CONST SOME_CONST = true;
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
